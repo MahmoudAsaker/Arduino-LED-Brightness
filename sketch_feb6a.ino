51fcc76b65bb323bae027e55aa4b138fb09b87f2
Mahmoud Asaker <158005079+MahmoudAsaker@users.noreply.github.com>
2024-02-06 13:57:56 +0200

    Add files via upload

---
+++ sketch_feb6a.ino
@@ -0,0 +1,18 @@
+
+int led=3;
+void setup()
+{
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  for(int i=0;i<=255;i=i+5){
+  analogWrite(led,i);
+  delay(50);
+}
+  for(int i=255;i>=0;i=i-5){
+  analogWrite(led,i);
+  delay(50);
+  }
+}
